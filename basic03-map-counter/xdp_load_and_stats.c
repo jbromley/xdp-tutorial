3ec9a19b2b01bcf824df2445bccee309f8977a50
J. Bromley <jay@appliedinvention.com>
2021-10-04 12:10:57 -0700

    Added stats and per-cpu map.
    Compile statistics for dropped and aborted packages. Also use a per-cpu map to
    avoid having to do manual locking.

--- basic03-map-counter/xdp_load_and_stats.c
+++ basic03-map-counter/xdp_load_and_stats.c
@@ -116,28 +116,30 @@ static void stats_print(struct stats_record *stats_rec,
 			struct stats_record *stats_prev)
 {
 	struct record *rec, *prev;
-	double period;
-	__u64 packets;
-	double pps; /* packets per sec */
 
 	/* Assignment#2: Print other XDP actions stats  */
+        for (int action_index = 0; action_index < XDP_ACTION_MAX; action_index++)
 	{
 		char *fmt = "%-12s %'11lld pkts (%'10.0f pps)"
-			//" %'11lld Kbytes (%'6.0f Mbits/s)"
+			" %'11lld Kbytes (%'6.0f Mbits/s)"
 			" period:%f\n";
 		const char *action = action2str(XDP_PASS);
-		rec  = &stats_rec->stats[0];
-		prev = &stats_prev->stats[0];
+		rec  = &stats_rec->stats[action_index];
+		prev = &stats_prev->stats[action_index];
 
-		period = calc_period(rec, prev);
+		double period = calc_period(rec, prev);
 		if (period == 0)
 		       return;
 
-		packets = rec->total.rx_packets - prev->total.rx_packets;
-		pps     = packets / period;
+		__u64 packets = rec->total.rx_packets - prev->total.rx_packets;
+		double pps = packets / period;
 
-		printf(fmt, action, rec->total.rx_packets, pps, period);
+                __u64 bytes = rec->total.rx_bytes - prev->total.rx_bytes;
+                double bps = bytes / period;
+
+		printf(fmt, action, rec->total.rx_packets, pps, rec->total.rx_bytes, bps, period);
 	}
+        printf("\n");
 }
 
 /* BPF_MAP_TYPE_ARRAY */
@@ -153,10 +155,22 @@ void map_get_value_array(int fd, __u32 key, struct datarec *value)
 void map_get_value_percpu_array(int fd, __u32 key, struct datarec *value)
 {
 	/* For percpu maps, userspace gets a value per possible CPU */
-	// unsigned int nr_cpus = bpf_num_possible_cpus();
-	// struct datarec values[nr_cpus];
+	unsigned int nr_cpus = bpf_num_possible_cpus();
+	struct datarec values[nr_cpus];
 
-	fprintf(stderr, "ERR: %s() not impl. see assignment#3", __func__);
+        if (bpf_map_lookup_elem(fd, &key, values) != 0) {
+                fprintf(stderr, "ERR: bpr_map_lookup_elem failed on key: 0x%X\n", key);
+                return;
+        }
+
+        __u64 sum_bytes = 0;
+        __u64 sum_pkts = 0;
+        for (int i = 0; i < nr_cpus; i++) {
+                sum_pkts += values[i].rx_packets;
+                sum_bytes += values[i].rx_bytes;
+        }
+        value->rx_packets = sum_pkts;
+        value->rx_bytes = sum_bytes;
 }
 
 static bool map_collect(int fd, __u32 map_type, __u32 key, struct record *rec)
@@ -171,7 +185,8 @@ static bool map_collect(int fd, __u32 map_type, __u32 key, struct record *rec)
 		map_get_value_array(fd, key, &value);
 		break;
 	case BPF_MAP_TYPE_PERCPU_ARRAY:
-		/* fall-through */
+                map_get_value_percpu_array(fd, key, &value);
+                break;
 	default:
 		fprintf(stderr, "ERR: Unknown map_type(%u) cannot handle\n",
 			map_type);
@@ -181,6 +196,7 @@ static bool map_collect(int fd, __u32 map_type, __u32 key, struct record *rec)
 
 	/* Assignment#1: Add byte counters */
 	rec->total.rx_packets = value.rx_packets;
+        rec->total.rx_bytes = value.rx_bytes;
 	return true;
 }
 
@@ -188,9 +204,9 @@ static void stats_collect(int map_fd, __u32 map_type,
 			  struct stats_record *stats_rec)
 {
 	/* Assignment#2: Collect other XDP actions stats  */
-	__u32 key = XDP_PASS;
-
-	map_collect(map_fd, map_type, key, &stats_rec->stats[0]);
+        for (__u32 key = 0; key < XDP_ACTION_MAX; key++) {
+                map_collect(map_fd, map_type, key, &stats_rec->stats[key]);
+        }
 }
 
 static void stats_poll(int map_fd, __u32 map_type, int interval)
